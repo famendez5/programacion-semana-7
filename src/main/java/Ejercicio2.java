912c6759878abdc37d48011d4b1e26da57225580
Fabian Mendez <famendez5@poligran.edu.co>
2020-04-29 12:13:09 -0500

    Se crea una clase para cada ejercicio

---
+++ src/main/java/Ejercicio2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.print("Ingrese abc: ");
+
+		}
+	}
+}
---
+++ src/main/java/Ejercicio7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.print("Ingrese abc: ");
+
+		}
+	}
+}
