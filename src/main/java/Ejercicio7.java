4b0823ca8b40bdfb71d890d7b0fac5903ff32fad
twopi <twopi@DESKTOP-UTSI85O>
2020-04-29 19:34:01 -0500

    Holaaaa Compañeros Gracias... prueba 2

--- src/main/java/Ejercicio7.java
+++ src/main/java/Ejercicio7.java
@@ -4,8 +4,12 @@ public class Ejercicio7 {
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
 			System.out.print("Ingrese abc: ");
-
+            int Fmendez = Genius;
+            int Natalia = Juya;
+            int YO = nOOb jeje;
+            System.out.print("Jjejejejej");
+            		
 		}
 	}
 }
-//dfdfd//
+//Hola compañeros Gracias//
